86fcf4a79cd7f72283f55159fbca3e030865a1b2
koome <leonkoome4@gmail.com>
2026-05-08 10:56:03 +0300

    clean css structure fix 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Navbar from "./components/Navbar"
+import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import ProjectDetails from "./pages/ProjectDetails"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<ProjectDetails />} />
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
